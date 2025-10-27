46eeac96085ebf6ea85dc3f3db093af0739ac521
Leap <git@leap.new>
2025-10-27 13:43:38 +0000

    Move item above competency heatmap

--- frontend/pages/OverviewPage.tsx
+++ frontend/pages/OverviewPage.tsx
@@ -47,12 +47,13 @@ export function OverviewPage() {
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-8">
-          <div className="lg:col-span-2">
-            <AnalyticsSummary />
-          </div>
           <div>
             <ReadinessOverview students={students} />
           </div>
+        </div>
+
+        <div className="mb-8">
+          <AnalyticsSummary />
         </div>
 
         <div className="mb-8">
